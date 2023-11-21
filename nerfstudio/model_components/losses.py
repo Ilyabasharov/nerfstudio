abfff69760f9ee549bdddcba62c8e0dbae1d882b
Ilya Basharov <ilya.basharov.98@mail.ru>
2023-11-21 15:37:44 +0000

    [Enhance] add efficient dist loss

--- nerfstudio/model_components/losses.py
+++ nerfstudio/model_components/losses.py
@@ -30,7 +30,7 @@ from nerfstudio.utils.math import (
     sorted_interp_quad,
     blur_stepfun,
 )
-from nerfstudio.utils.misc import Numeric
+from nerfstudio.utils.misc import Numeric, torch_compile
 
 L1Loss = nn.L1Loss
 MSELoss = nn.MSELoss
@@ -44,6 +44,7 @@ URF_SIGMA_SCALE_FACTOR = 3.0
 FORCE_PSEUDODEPTH_LOSS = False
 
 
+@torch_compile(dynamic=True, mode="reduce-overhead")
 def outer(
     t0_starts: Float[Tensor, "*batch num_samples_0"],
     t0_ends: Float[Tensor, "*batch num_samples_0"],
@@ -63,12 +64,20 @@ def outer(
         t1_ends: end of the interval edges
         y1: weights
     """
-    cy1 = torch.cat([torch.zeros_like(y1[..., :1]), torch.cumsum(y1, dim=-1)], dim=-1)
+    cy1 = torch.cat([y1.new_zeros(*y1.shape[:-1], 1), y1.cumsum(dim=-1)], dim=-1)
+    max_idx = y1.shape[-1] - 1
 
-    idx_lo = torch.searchsorted(t1_starts.contiguous(), t0_starts.contiguous(), side="right") - 1
-    idx_lo = torch.clamp(idx_lo, min=0, max=y1.shape[-1] - 1)
-    idx_hi = torch.searchsorted(t1_ends.contiguous(), t0_ends.contiguous(), side="right")
-    idx_hi = torch.clamp(idx_hi, min=0, max=y1.shape[-1] - 1)
+    idx_lo = (
+        torch.searchsorted(t1_starts.contiguous(), t0_starts.contiguous(), side="right")
+        .sub_(1)
+        .clamp_(min=0, max=max_idx)
+    )
+
+    idx_hi = (
+        torch.searchsorted(t1_ends.contiguous(), t0_ends.contiguous(), side="right")
+        .clamp_(min=0, max=max_idx)
+    )
+
     cy1_lo = torch.take_along_dim(cy1[..., :-1], idx_lo, dim=-1)
     cy1_hi = torch.take_along_dim(cy1[..., 1:], idx_hi, dim=-1)
     y0_outer = cy1_hi - cy1_lo
@@ -76,6 +85,7 @@ def outer(
     return y0_outer
 
 
+@torch_compile(dynamic=True, mode="reduce-overhead")
 def lossfun_outer(
     t: Float[Tensor, "*batch num_samples_1"],
     w: Float[Tensor, "*batch num_samples"],
@@ -109,6 +119,7 @@ def interlevel_loss(weights_list, ray_samples_list) -> Tensor:
     loss_interlevel = 0.0
     for ray_samples, weights in zip(ray_samples_list[:-1], weights_list[:-1]):
         sdist = ray_samples.to_sdist()
+        assert sdist is not None
         cp = sdist  # (num_rays, num_samples + 1)
         wp = weights[..., 0]  # (num_rays, num_samples)
         loss_interlevel += torch.mean(lossfun_outer(c, w, cp, wp))
@@ -127,7 +138,9 @@ def zipnerf_loss(
     # This implementation matches ZipNeRF up to the scale.
     # In the paper the loss is computed as the sum over the ray samples.
     # Here we take the mean and the multiplier for this loss should be changed accordingly.
-    c = ray_samples_list[-1].to_sdist().detach()
+    c = ray_samples_list[-1].to_sdist()
+    assert c is not None
+    c = c.detach()
     w = weights_list[-1][..., 0].detach()
 
     w_norm = w / (c[..., 1:] - c[..., :-1])
@@ -139,7 +152,7 @@ def zipnerf_loss(
 
         # piecewise linear pdf to piecewise quadratic cdf
         area = 0.5 * (w_[..., 1:] + w_[..., :-1]) * (c_[..., 1:] - c_[..., :-1])
-        cdf = torch.cat([torch.zeros_like(area[..., :1]), torch.cumsum(area, dim=-1)], dim=-1)
+        cdf = torch.cat([area.new_zeros(*area.shape[:-1], 1), area.cumsum(dim=-1)], dim=-1)
 
         # query piecewise quadratic interpolation
         cdf_interp = sorted_interp_quad(cp, c_, w_, cdf)
@@ -153,28 +166,69 @@ def zipnerf_loss(
 
 
 # Verified
-def lossfun_distortion(t, w):
+@torch_compile(dynamic=True, mode="reduce-overhead")
+def lossfun_distortion(
+    t: Float[Tensor, "*batch num_samples+1"],
+    w: Float[Tensor, "*batch num_samples"],
+) -> Tensor:
     """
     https://github.com/kakaobrain/NeRF-Factory/blob/f61bb8744a5cb4820a4d968fb3bfbed777550f4a/src/model/mipnerf360/helper.py#L142
     https://github.com/google-research/multinerf/blob/b02228160d3179300c7d499dca28cb9ca3677f32/internal/stepfun.py#L266
-    """
-    ut = (t[..., 1:] + t[..., :-1]) / 2
-    dut = torch.abs(ut[..., :, None] - ut[..., None, :])
-    loss_inter = torch.sum(w * torch.sum(w[..., None, :] * dut, dim=-1), dim=-1)
 
-    loss_intra = torch.sum(w**2 * (t[..., 1:] - t[..., :-1]), dim=-1) / 3
+    Original O(N^2) realization of distortion loss from MipNeRF 360.
+    There are B rays each with N sampled points.
+    t: Samples in ray of shape [B, N + 1]. Volume rendering weights of each point.
+    w: Float tensor in shape [B, N]. Volume rendering weights of each point.
+    """
+    ut = (t[..., 1:] + t[..., :-1]) / 2  # [B, N] midpoint
+    it = t[..., 1:] - t[..., :-1]  # [B, N] interval
+    dut = torch.abs(ut[..., :, None] - ut[..., None, :])  # [B, N, N] mm
+    loss_inter = torch.sum(w * torch.sum(w[..., None, :] * dut, dim=-1), dim=-1) # ww
+    loss_intra = torch.sum(w**2 * it, dim=-1) / 3
+
+    return loss_inter + loss_intra
+
+
+# Verified
+@torch_compile(dynamic=True, mode="reduce-overhead")
+def eff_lossfun_distortion(
+    t: Float[Tensor, "*batch num_samples+1"],
+    w: Float[Tensor, "*batch num_samples"],
+) -> Tensor:
+    """
+    https://github.com/sunset1995/torch_efficient_distloss/blob/03ea697ca261e839cae10e2ccc25cb350c04d226/torch_efficient_distloss/eff_distloss.py#L16
+
+    Efficient O(N) and Faster realization of distortion loss from MipNeRF 360.
+    There are B rays each with N sampled points.
+    t: Samples in ray of shape [B, N + 1]. Volume rendering weights of each point.
+    w: Float tensor in shape [B, N]. Volume rendering weights of each point.
+    """
+    ut = (t[..., 1:] + t[..., :-1]) / 2  # [B, N] midpoint
+    it = t[..., 1:] - t[..., :-1]  # [B, N] interval
+
+    loss_intra = torch.sum(w**2 * it, dim=-1) / 3
+
+    wm = w * ut
+    w_cumsum = w.cumsum(dim=-1)
+    wm_cumsum = wm.cumsum(dim=-1)
+    loss_inter_0 = wm[..., 1:] * w_cumsum[..., :-1]
+    loss_inter_1 = w[..., 1:] * wm_cumsum[..., :-1]
+    loss_inter = 2 * (loss_inter_0 - loss_inter_1).sum(dim=-1)
 
     return loss_inter + loss_intra
 
 
 def distortion_loss(
-    weights_list: List[Tensor],
+    weights_list: List[Float[Tensor, "*batch num_samples"]],
     ray_samples_list: List[RaySamples],
 ) -> Tensor:
     """From mipnerf360"""
-    c = ray_samples_list[-1].to_sdist()
-    w = weights_list[-1][..., 0]
-    loss = lossfun_distortion(c, w).mean()
+    loss = 0
+    for weight, ray_samples in zip(weights_list, ray_samples_list):
+        c = ray_samples.to_sdist()
+        w = weight[..., 0]
+        loss += eff_lossfun_distortion(c, w).mean()
+    assert isinstance(loss, Tensor)
     return loss
 
 
@@ -563,16 +617,6 @@ def decay_loss(
         regularize_fn(decay).mean()
         for decay in decay_list
     )  # type: ignore
-
-    return loss
-
-
-
-def hash_decay_loss(
-    hash_decay_list: List[Float[Tensor, "0"]],
-) -> Float[Tensor, "0"]:
-    """Implementation of hash decay loss."""
-    loss: Tensor = sum(hash_decay.mean() for hash_decay in hash_decay_list)  # type: ignore
 
     return loss
 
